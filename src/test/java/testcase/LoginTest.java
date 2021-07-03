291d0a5ef53f36dc7d6a613564a51dd521d6173d
“hhy” <1352963143@qq.com>
2021-07-03 23:24:50 +0800

    fisrt commit

---
+++ src/test/java/testcase/LoginTest.java
@@ -0,0 +1,16 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Auther: iteration.0
+ * @Date: 3/7/2021 - 07 - 03 - 10:35 PM
+ * @Description: testcase
+ * @version: 1.0
+ */
+public class LoginTest {
+    @Test
+    public void  loginTest() {
+        System.out.println("Login  success");
+    }
+}
---
+++ src/test/java/testcase/RegisterTest.java
@@ -0,0 +1,16 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Auther: iteration.0
+ * @Date: 3/7/2021 - 07 - 03 - 10:35 PM
+ * @Description: testcase
+ * @version: 1.0
+ */
+public class RegisterTest {
+    @Test
+    public void registerTest() {
+        System.out.println(" Register   success 注册成功");
+    }
+}
