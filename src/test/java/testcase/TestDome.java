0591f4b8d4e2684a20d6617e78be0a3eb0f64d6c
“hhy” <1352963143@qq.com>
2021-07-04 08:04:37 +0800

    jint

---
+++ src/test/java/testcase/TestDome.java
@@ -0,0 +1,14 @@
+package testcase;
+import org.testng.annotations.Test;
+/**
+ * @Auther: iteration.0
+ * @Date: 4/7/2021 - 07 - 04 - 7:50 AM
+ * @Description: testcase
+ * @version: 1.0
+ */
+public class TestDome {
+    @Test
+    public void  test(){
+        System.out.println("这个项目去Git哪里了");
+    }
+}
